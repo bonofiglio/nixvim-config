523d6332e0b549eaf9ff43cd43f5fa9772d752ec
Daniel Bonofiglio <dev@dan.uy>
2024-02-14 13:31:20 -0300

    fix: move codeium keymaps to native nvim keymaps

--- config/plugins/codeium.nix
+++ config/plugins/codeium.nix
@@ -2,11 +2,52 @@
   plugins.codeium-vim = {
     enable = true;
     noMapTab = true;
-    keymaps = {
-      accept = "<C-.>";
-      next = "<c-m>";
-      prev = "<c-,>";
-      clear = "<c-n>";
-    };
   };
+
+  keymaps = [
+    {
+      mode = "i";
+      key = "<c-.>";
+      action = "function() return vim.fn['codeium#Accept']() end";
+      lua = true;
+      options = {
+        desc = "Accept codeium suggestion";
+        expr = true;
+        silent = true;
+      };
+    }
+    {
+      mode = "i";
+      key = "<c-,>";
+      action = "function() return vim.fn['codeium#CycleCompletions'](-1) end";
+      lua = true;
+      options = {
+        desc = "Next codeium suggestion";
+        expr = true;
+        silent = true;
+      };
+    }
+    {
+      mode = "i";
+      key = "<c-m>";
+      action = "function () return vim.fn['codeium#CycleCompletions'](1) end";
+      lua = true;
+      options = {
+        desc = "Previous codeium suggestion";
+        expr = true;
+        silent = true;
+      };
+    }
+    {
+      mode = "i";
+      key = "<c-n>";
+      action = "function() return vim.fn['codeium#Clear']() end";
+      lua = true;
+      options = {
+        desc = "Clear codeium suggestion";
+        expr = true;
+        silent = true;
+      };
+    }
+  ];
 }
