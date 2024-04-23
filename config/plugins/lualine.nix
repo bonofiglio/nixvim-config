dae0529ab156d50b9bc6c19217c101711f887c44
Daniel Bonofiglio <dev@dan.uy>
2024-04-23 12:23:11 -0300

    add macro recording to lualine

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -25,8 +25,28 @@
     sections = {
       lualine_a = [ "mode" ];
       lualine_b = [ "branch" ];
-      lualine_c = [ ];
-      lualine_x = [ "diff" ];
+      lualine_c = [{
+        name = "filename";
+        extraConfig = {
+          file_status = true; # Displays file status (readonly status, modified status)
+          newfile_status = false; # Display new file status (new file means no write after created)
+          path = 1; # 0: Just the filename
+          #           1: Relative path
+          #           2: Absolute path
+          #           3: Absolute path; with tilde as the home directory
+          #           4: Filename and parent dir; with tilde as the home directory
+
+          shorting_target = 40; # Shortens path to leave 40 spaces in the window
+          #                       for other components. (terrible name; any suggestions?)
+          symbols = {
+            modified = "[+]"; # Text to show when the file is modified.
+            readonly = "[-]"; # Text to show when the file is non-modifiable or readonly.
+            unnamed = "[No Name]"; # Text to show for unnamed buffers.
+            newfile = "[New]"; # Text to show for newly created file before first write
+          };
+        };
+      }];
+      lualine_x = [ ];
       lualine_y = [ "encoding" "filetype" ];
       lualine_z = [ "location" ];
     };
@@ -39,4 +59,26 @@
       lualine_z = [ ];
     };
   };
+
+  extraConfigLua = ''
+    require("lualine").setup({
+      sections = {
+        lualine_x = {
+          {
+            "diff"
+          },
+          {
+            require("noice").api.statusline.mode.get,
+            cond = function()
+                local status = require("noice").api.statusline.mode.get()
+                local prefix = "recording "
+
+                return status and status:sub(1, #prefix) == prefix
+            end,
+            color = { fg = "#fff" },
+          }
+        },
+      },
+    })
+  '';
 }
