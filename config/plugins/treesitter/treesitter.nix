5d784135569ea5c0929db336f6f809288d631574
Daniel Bonofiglio <dev@dan.uy>
2024-07-14 03:25:06 -0300

    fix: use treesitter for syntax highlighting again

--- config/plugins/treesitter/treesitter.nix
+++ config/plugins/treesitter/treesitter.nix
@@ -4,14 +4,12 @@
     nixGrammars = true;
     nixvimInjections = true;
     settings = {
-      auto_intsall = true;
+      highlight = {
+        enable = true;
+      };
+      auto_install = true;
+      ensure_installed = "all";
       parser_install_dir = "~/.cache/nvim/treesitters";
     };
   };
-
-  extraConfigLuaPre = '' 
-    local treesitter_dir = vim.fn.stdpath("cache") .. "/treesitters"
-    vim.fn.mkdir(treesitter_dir, "p")
-    vim.opt.runtimepath:append(treesitter_dir)
-  ''; # Add treesitter parser directory to runtimepath
 }
