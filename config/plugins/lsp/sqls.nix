8193045f24f7541bf6c6dcd4c33ceb6113c2a074
Daniel Bonofiglio <dev@dan.uy>
2024-08-28 17:32:54 -0300

    fix: disable sqls formatting

--- config/plugins/lsp/sqls.nix
+++ config/plugins/lsp/sqls.nix
@@ -1,5 +1,9 @@
 {
   plugins.lsp.servers.sqls = {
     enable = true;
+    onAttach.function = ''
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+    '';
   };
 }
