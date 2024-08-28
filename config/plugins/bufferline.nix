c2836f7d6402720b2a76da21f9d6dbf395a11cd3
Daniel Bonofiglio <dev@dan.uy>
2024-08-28 10:24:05 -0300

    update bufferline option schema

--- config/plugins/bufferline.nix
+++ config/plugins/bufferline.nix
@@ -1,105 +1,110 @@
 {
   plugins.bufferline = {
     enable = true;
-    showBufferCloseIcons = true;
-    separatorStyle = "thick";
-    diagnostics = "nvim_lsp";
-    diagnosticsIndicator = ''function(count)
-      return "(" .. count .. ")"
-    end'';
-    indicator = {
-      icon = "▎";
-      style = "icon";
-    };
 
-    highlights = {
-      bufferVisible = {
-        italic = false;
-        bold = false;
+    settings = {
+      options = {
+        show_buffer_close_icons = true;
+        separator_style = "thick";
+        diagnostics = "nvim_lsp";
+        diagnostics_indicator = ''function(count)
+            return "(" .. count .. ")"
+            end'';
+        indicator = {
+          icon = "▎";
+          style = "icon";
+        };
       };
-      bufferSelected = {
-        italic = false;
-        bold = false;
-      };
-      numbersSelected = {
-        italic = false;
-        bold = false;
-      };
-      diagnosticSelected = {
-        italic = false;
-        bold = false;
-      };
-      hintVisible = {
-        italic = false;
-        bold = false;
-      };
-      hintSelected = {
-        italic = false;
-        bold = false;
-      };
-      hintDiagnosticSelected = {
-        italic = false;
-        bold = false;
-      };
-      infoVisible = {
-        italic = false;
-        bold = false;
-      };
-      infoSelected = {
-        italic = false;
-        bold = false;
-      };
-      infoDiagnosticSelected = {
-        italic = false;
-        bold = false;
-      };
-      warningVisible = {
-        italic = false;
-        bold = false;
-      };
-      warningSelected = {
-        italic = false;
-        bold = false;
-      };
-      warningDiagnosticSelected = {
-        italic = false;
-        bold = false;
-      };
-      errorVisible = {
-        italic = false;
-        bold = false;
-      };
-      errorSelected = {
-        italic = false;
-        bold = false;
-      };
-      errorDiagnosticSelected = {
-        italic = false;
-        bold = false;
-      };
-      duplicateSelected = {
-        italic = false;
-        bold = false;
-      };
-      duplicateVisible = {
-        italic = false;
-        bold = false;
-      };
-      duplicate = {
-        italic = false;
-        bold = false;
-      };
-      pickSelected = {
-        italic = false;
-        bold = false;
-      };
-      pickVisible = {
-        italic = false;
-        bold = false;
-      };
-      pick = {
-        italic = false;
-        bold = false;
+
+      highlights = {
+        buffer_visible = {
+          italic = false;
+          bold = false;
+        };
+        buffer_selected = {
+          italic = false;
+          bold = false;
+        };
+        numbers_selected = {
+          italic = false;
+          bold = false;
+        };
+        diagnostic_selected = {
+          italic = false;
+          bold = false;
+        };
+        hint_visible = {
+          italic = false;
+          bold = false;
+        };
+        hint_selected = {
+          italic = false;
+          bold = false;
+        };
+        hint_diagnostic_selected = {
+          italic = false;
+          bold = false;
+        };
+        info_visible = {
+          italic = false;
+          bold = false;
+        };
+        info_selected = {
+          italic = false;
+          bold = false;
+        };
+        info_diagnostic_selected = {
+          italic = false;
+          bold = false;
+        };
+        warning_visible = {
+          italic = false;
+          bold = false;
+        };
+        warning_selected = {
+          italic = false;
+          bold = false;
+        };
+        warning_diagnostic_selected = {
+          italic = false;
+          bold = false;
+        };
+        error_visible = {
+          italic = false;
+          bold = false;
+        };
+        error_selected = {
+          italic = false;
+          bold = false;
+        };
+        error_diagnostic_selected = {
+          italic = false;
+          bold = false;
+        };
+        duplicate_selected = {
+          italic = false;
+          bold = false;
+        };
+        duplicate_visible = {
+          italic = false;
+          bold = false;
+        };
+        duplicate = {
+          italic = false;
+          bold = false;
+        };
+        pick_selected = {
+          italic = false;
+          bold = false;
+        };
+        pick_visible = {
+          italic = false;
+          bold = false;
+        };
+        pick = {
+          italic = false;
+          bold = false;
+        };
       };
     };
   };
